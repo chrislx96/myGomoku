08663541f7268e440da49e3dad4fbabf59d1c8d1
chrislx96 <christopher.lee.96.ks@gmail.com>
2020-10-20 22:51:56 +1100

    config

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return (
+        <main>
+          <h1>Gomoku</h1>
+        </main>
+      );
+    }
+  }
+  export default App;
